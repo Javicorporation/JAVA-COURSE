8438d2cb95599e9959307ae5d54fa26d6323abca
Javicorporation <krchipullajavier@gmail.com>
2024-03-18 21:28:59 -0500

    primer proyecto

---
+++ AplicacionJava/src/aplicacionjava/AplicacionJava.java
@@ -0,0 +1,11 @@
+package aplicacionjava;
+
+public class AplicacionJava {
+
+    public static void main(String[] args) {
+        System.out.println("Que mas");
+        System.out.println("Rarita");
+        System.out.println("jajajaj");
+    }
+    
+}
