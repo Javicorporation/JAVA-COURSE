5385aa1e61b3e37a7c7e0fc12c24e3835475fd05
Javicorporation <krchipullajavier@gmail.com>
2024-03-19 22:37:30 -0500

    variables

---
+++ AplicacionVariables/src/aplicacionvariables/AplicacionVariables.java
@@ -0,0 +1,23 @@
+package aplicacionvariables;
+
+public class AplicacionVariables {
+
+    public static void main(String[] args) {
+        // enterios   int: 1 - 2 - 3 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 0
+        int numero1 = 1;
+        System.out.println(numero1);
+        // decimales  float: 1,2 - 2,5 - 7,8 
+        
+        
+        // decimales largos double:  1,2 - 2,5 - 7,8
+        
+        // cadena de caracteres String: "que mas ve"
+        
+        // caracteres Char: "a", "b", "A"
+        
+        // valores booleanos boolean; true - false
+        
+        
+    }
+    
+}
