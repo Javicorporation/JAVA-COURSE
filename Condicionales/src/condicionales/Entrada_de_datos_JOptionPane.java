955bc0f8b3250cd8b160fc7383ebf81ffd85dbf3
Javicorporation <krchipullajavier@gmail.com>
2024-03-21 23:17:56 -0500

    ingreso con JOptionPane cierre

--- Condicionales/src/condicionales/Entrada_de_datos_JOptionPane.java
+++ Condicionales/src/condicionales/Entrada_de_datos_JOptionPane.java
@@ -15,8 +15,16 @@ public class Entrada_de_datos_JOptionPane {
        char caracter;
        
        numeroEntero = Integer.parseInt(JOptionPane.showInputDialog("escribe un numero entero: "));
+       numeroDecimalGrande = Double.parseDouble(JOptionPane.showInputDialog("escribe un decimal: "));
+       numeroDecimal = Float.parseFloat(JOptionPane.showInputDialog("escribe un numero flotante: "));
+       texto = JOptionPane.showInputDialog("escribe una frace: ");
+       caracter = JOptionPane.showInputDialog("escribe un caracter: ").charAt(0);
        
        JOptionPane.showMessageDialog(null,"este es el numero: "+numeroEntero);
+       JOptionPane.showMessageDialog(null, "este es el decimal: "+numeroDecimalGrande);
+       JOptionPane.showMessageDialog(null,"este es el valor flotante: "+numeroDecimal);
+       JOptionPane.showMessageDialog(null, "este es el texto: "+texto);
+       JOptionPane.showMessageDialog(null,"este es el caracter "+caracter);
     }
     
 }
