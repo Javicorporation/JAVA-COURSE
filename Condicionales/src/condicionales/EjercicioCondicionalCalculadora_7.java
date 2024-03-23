2fa313fd7ebacb69da437b7f34ec8d2b8130841b
Javicorporation <krchipullajavier@gmail.com>
2024-03-22 22:11:14 -0500

    ejercicio de condicional 7 sin terminar

---
+++ Condicionales/src/condicionales/EjercicioCondicionalCalculadora_7.java
@@ -0,0 +1,42 @@
+package condicionales;
+
+import java.util.Scanner;
+
+
+public class EjercicioCondicionalCalculadora_7 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+        int numero1;
+        int numero2;
+        int opcion;
+        
+        System.out.println("MENU");
+        
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+        System.out.println("5. Raiz cuadrada");
+        System.out.println("6. Raiz cubica");
+        System.out.println("7. Potencia");
+        System.out.println("8. Porcentaje");
+        System.out.println("9. Salir");
+        System.out.print("escribe una opcion: ");
+        opcion = entrada.nextInt();
+        
+        switch (opcion) {
+            case val:
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
+        
+        
+    }
+    
+}
