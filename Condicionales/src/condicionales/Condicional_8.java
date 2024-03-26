6f34e23b9ffc06fcdf2e7c2e9b1ab95f9dc517e2
Javicorporation <krchipullajavier@gmail.com>
2024-03-25 21:32:26 -0500

    creacion de clase Condicional_8 de numero mayor de 5 numeros

--- Condicionales/src/condicionales/Condicional_8.java
+++ Condicionales/src/condicionales/Condicional_8.java
@@ -1,8 +1,42 @@
 package condicionales;
 
+import java.util.Scanner;
+
 public class Condicional_8 {
     public static void main (String[] args){
-        System.out.println("bien");
+
+        Scanner estrada = new Scanner(System.in);
+
+        int numero1;
+        int numero2;
+        int numero3;
+        int numero4;
+        int numero5;
+
+        System.out.println("Escriba el primer numero: ");
+        numero1 = estrada.nextInt();
+        System.out.println("Escriba el segundo numero: ");
+        numero2 = estrada.nextInt();
+        System.out.println("Escriba el tercer numero: ");
+        numero3 = estrada.nextInt();
+        System.out.println("Escriba el cuarto numero: ");
+        numero4 = estrada.nextInt();
+        System.out.println("Escriba el quinto numero: ");
+        numero5 = estrada.nextInt();
+
+        if(numero1 > numero2 && numero1 > numero3 && numero1> numero4 && numero1 > numero5){
+            System.out.printf("el numero mayor es el numero %d", numero1);
+        }else if (numero2 > numero1 && numero2 > numero3 && numero2> numero4 && numero2 > numero5){
+            System.out.printf("el numero mayor es el numero %d", numero2);
+        }else if (numero3 > numero2 && numero3 > numero1 && numero3> numero4 && numero3 > numero5){
+            System.out.printf("el numero mayor es el numero %d", numero3);
+        }else if (numero4 > numero2 && numero4 > numero3 && numero4> numero1 && numero4 > numero5){
+            System.out.printf("el numero mayor es el numero %d", numero4);
+        }else if (numero5 > numero2 && numero5 > numero3 && numero5> numero4 && numero5 > numero1){
+            System.out.printf("el numero mayor es el numero %d", numero5);
+        }
+
+
 
     }
 }
