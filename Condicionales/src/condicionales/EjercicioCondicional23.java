ad9edfd0ae3ae65c1a6af48dc630c5047ded0b19
Javicorporation <krchipullajavier@gmail.com>
2024-03-28 22:33:09 -0500

    clase 23 ejercicio condicional "plano carteciano", creado, estoy cansado XD.

---
+++ Condicionales/src/condicionales/EjercicioCondicional23.java
@@ -0,0 +1,13 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class EjercicioCondicional23 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int numero1;
+        int numero2;
+
+    }
+}
