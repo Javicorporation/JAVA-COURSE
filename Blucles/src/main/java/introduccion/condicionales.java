a55e3a5b4d0ac20dfd8f3f65bbddd2fbde6375e2
Javicorporation <krchipullajavier@gmail.com>
2024-03-29 17:41:48 -0500

    ejercicio de bucle while

---
+++ Blucles/src/main/java/introduccion/condicionales.java
@@ -0,0 +1,21 @@
+
+package introduccion;
+
+import java.util.Scanner;
+
+
+public class condicionales {
+
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int edad = 10;
+        while (edad <= 20) {    
+            System.out.println("mensaje de edad");
+            edad++;
+            
+        }
+        
+    }
+    
+}
