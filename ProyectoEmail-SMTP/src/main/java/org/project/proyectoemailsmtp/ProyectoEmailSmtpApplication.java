25b3586a5bcfabbf4d58574c675467af253a2b69
Javicorporation <krchipullajavier@gmail.com>
2024-04-07 16:16:54 -0500

    firts commit

---
+++ ProyectoEmail-SMTP/src/main/java/org/project/proyectoemailsmtp/ProyectoEmailSmtpApplication.java
@@ -0,0 +1,13 @@
+package org.project.proyectoemailsmtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEmailSmtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoEmailSmtpApplication.class, args);
+    }
+
+}
---
+++ ProyectoEmail-SMTP/src/test/java/org/project/proyectoemailsmtp/ProyectoEmailSmtpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.proyectoemailsmtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEmailSmtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
