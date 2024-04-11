b379f5cb31e1d36977495e982316a232a7c6635c
Javicorporation <krchipullajavier@gmail.com>
2024-04-11 16:52:11 -0500

    ended excercise 9 of arrays

--- Arrays/src/Excercise9Arrays.java
+++ Arrays/src/Excercise9Arrays.java
@@ -4,17 +4,34 @@ public class Excercise9Arrays {
     public static void main(String[] args) {
         Scanner getting = new Scanner(System.in);
 
-        String array[] = new String[4];
+        int lengthN;
+        String arrayName[];
+        int arrayAge[];
 
-        array[0] = "hola";
-        array[1] = "holaa";
-        array[2] = "holaaa";
-        array[3] = "holaaaa";
+        System.out.print("Write a Length of array: ");
+        lengthN = getting.nextInt();
+        getting.nextLine();
+        arrayName = new String[lengthN];
+        arrayAge = new int[lengthN];
 
-        for (int i = 0; i< array.length; i++){
-            System.out.println(array[i].length());
 
+        for (int i = 0; i< arrayName.length; i++){
+            System.out.print("Write a name "+(i+1)+": ");
+            arrayName[i] = getting.nextLine();
+
+            System.out.print("Write a age "+(i+1)   +": ");
+            arrayAge[i] = getting.nextInt();
+            getting.nextLine();
         }
+        getting.close();
+
+        for (int j = 0; j<arrayAge.length; j++){
+            if (arrayAge[j]>= 18) {
+                System.out.println("the age is: "+ arrayAge[j]);
+            }
+        }
+
+
 
     }
 }
