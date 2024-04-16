f231e1c09f968e0bcec51ce3c59e59d88b36786a
Javicorporation <krchipullajavier@gmail.com>
2024-04-16 16:20:23 -0500

    ended excercise 16_2 of arrays finished ok part2

--- Arrays/src/Excercise16_2Arrays.java
+++ Arrays/src/Excercise16_2Arrays.java
@@ -5,21 +5,37 @@ public class Excercise16_2Arrays {
     public static  void main(String[] args){
         Scanner getting = new Scanner(System.in);
 
-        ArrayList<Integer> arrayListMain = new ArrayList<>();
-        int number;
-        do {
+        int arrayMain[];
+        int lengthArraymain;
+        int index = 0;
+
+        System.out.print("write a length: ");
+        lengthArraymain = getting.nextInt();
+
+        arrayMain = new int[lengthArraymain];
+
+        while (true){
             System.out.print("write a number: ");
-            number = getting.nextInt();
-
-            if (number == -1){
-                System.out.println("EXIT");
+            int num = getting.nextInt();
+            if (num == -1) {
+                System.out.println("Exit");
                 break;
-            }else {
-                arrayListMain.add(number);
             }
-        }while (true);
+            if (index >= arrayMain.length) {
+                int newLength = arrayMain.length*2;
+                int[] newArray = new int[newLength];
+                System.arraycopy(arrayMain, 0, newArray, 0, arrayMain.length);
+                arrayMain = newArray;
+            }
+            arrayMain[index] = num;
+            index++;
+        }
+        System.out.println("Números ingresados:");
+        for (int i = 0; i < index; i++) {
+            System.out.print(arrayMain[i] + " ");
+        }
+        System.out.println();
 
-        System.out.println("the array is: "+arrayListMain);
-
+        getting.close();
     }
 }
