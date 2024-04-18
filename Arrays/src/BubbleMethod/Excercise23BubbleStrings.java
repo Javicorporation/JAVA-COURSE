e2843aa2ddaf174f578ea2ee647c68e85810cb2f
Javicorporation <krchipullajavier@gmail.com>
2024-04-18 18:38:54 -0500

    ended excercise 23 bubble of arrays finished ok part 2 ended

--- Arrays/src/BubbleMethod/Excercise23BubbleStrings.java
+++ Arrays/src/BubbleMethod/Excercise23BubbleStrings.java
@@ -3,14 +3,37 @@ package BubbleMethod;
 import java.util.Scanner;
 
 public class Excercise23BubbleStrings {
-    // entendi pero los resultados no los entendi
+    // entendi
     public static  void main(String[] args) {
         Scanner getting = new Scanner(System.in);
 
-        String cadena1 = "m";
-        String cadena2 = "mini";
+        int lengt;
+        String arrayStri[];
+        String box = "";
 
-        int resultado = cadena1.compareTo(cadena2);
-        System.out.println(resultado);
+        System.out.print("Write a length: ");
+        lengt = getting.nextInt();
+
+        arrayStri = new String[lengt];
+
+        for (int i = 0; i < arrayStri.length; i++){
+            System.out.print("Write a string: ");
+            arrayStri[i] = getting.next();
+        }
+        for (int i = 0 ; i < arrayStri.length-1; i++){
+            for (int j= 0; j < arrayStri.length-1; j++){
+                if (arrayStri[j].compareTo(arrayStri[j+1]) > 0) {
+                    box = arrayStri[j];
+                    arrayStri[j] = arrayStri[j+1];
+                    arrayStri[j+1] = box;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < arrayStri.length; i++){
+            System.out.println(arrayStri[i]);
+        }
+
     }
 }
