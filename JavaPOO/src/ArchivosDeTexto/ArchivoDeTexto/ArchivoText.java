a20e43acc26991446e5ebf0cbf6c0d7b7e964833
Javicorporation <krchipullajavier@gmail.com>
2024-07-05 14:39:00 -0500

    creacion de un archivo txt por medio de java el anterior fuen en una ruta relativa y la de ahora se creo en una ruta absoluta, tambien se pueden crear directorios.

--- JavaPOO/src/ArchivosDeTexto/ArchivoDeTexto/ArchivoText.java
+++ JavaPOO/src/ArchivosDeTexto/ArchivoDeTexto/ArchivoText.java
@@ -6,11 +6,16 @@ public class ArchivoText {
 
     public void crearArchivo() {
         // creamos un objeto de la clase File con un nombre ramdon
-        File archivo = new File("ArchivoDeTexto.txt");
+        // en una ruta relativa.
+        //File archivo = new File("ArchivoDeTexto.txt");
+        // en una ruta absoluta.
+        File archivo = new File("C:\\Users\\krchi\\OneDrive - Universidad de Guayaquil\\UG\\ArchivoDeTexto.txt");
+
 
         //ponemos un try catch para manejar las exceptions
         try {
             // ponemos un condicional para manejar el resultado la creaccion del archivo
+
             if (archivo.createNewFile()) {
                 System.out.println("El archivo se creo con exito");
             }else {
