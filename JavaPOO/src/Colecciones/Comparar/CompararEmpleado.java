acc28de0d26e8516ed53984e822408e748e547b2
Javicorporation <krchipullajavier@gmail.com>
2024-07-11 13:33:41 -0500

    Ejercicio de Listas de objetos - para ordenar objetos podemos crear una nueva clases para ordenar los objetos de una lista por medio de edad

--- JavaPOO/src/Colecciones/Comparar/CompararEmpleado.java
+++ JavaPOO/src/Colecciones/Comparar/CompararEmpleado.java
@@ -8,8 +8,19 @@ public class CompararEmpleado implements Comparator<Empleado> {
 
     // metodo de comparar 2 empleados con la implementacion de Comparator
     @Override
-    public int compare(Empleado o1, Empleado o2) {
+    public int compare(Empleado empl1, Empleado empl2) {
         // para ordenar objetos pos su nombre String
-        return o1.getNombre().compareTo(o2.getNombre());
+        //return o1.getNombre().compareTo(o2.getNombre());
+
+        // ordemanos por medio de la edad
+        int resultado = 0;
+        if (empl1.getEdad() > empl2.getEdad()) {
+            resultado = 1;
+        }else if(empl1.getEdad() < empl2.getEdad()) {
+            resultado = -1;
+        }else {
+            resultado = 0;
+        }
+        return resultado;
     }
 }
