ce3d36b88d0d248c1ab2f51b7a18a8b6f80a171a
Javicorporation <krchipullajavier@gmail.com>
2024-07-16 10:55:09 -0500

    implemantacion de treeSet

--- JavaPOO/src/ColeccionHashSet/Test.java
+++ JavaPOO/src/ColeccionHashSet/Test.java
@@ -2,6 +2,7 @@ package ColeccionHashSet;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
@@ -14,11 +15,27 @@ public class Test {
         conjuntosHash.add("Ecuador");
         conjuntosHash.add("Chile");
         conjuntosHash.add("Chile");
-        conjuntosHash.add("Chile");
-
+        conjuntosHash.add("");
 
         for (String valor: conjuntosHash){
             System.out.println("Pais: "+valor);
         }
+
+
+        /// uso de treeSet
+        // ordena los conjuntos de menor a mayor y tampoco repite los datos duplicados
+        Set<String> conjuntoTree = new TreeSet<>();
+        conjuntoTree.add("a-juan");
+        conjuntoTree.add("a-juan");
+        conjuntoTree.add("dari");
+        conjuntoTree.add("elia");
+        conjuntoTree.add("alan");
+        System.out.println();
+
+        for (String nombres: conjuntoTree){
+            System.out.println("Nombre: "+nombres);
+        }
+
+
     }
 }
