5f8f7b5789f2b63d1f0362c7864188258e7e5d3a
Javicorporation <krchipullajavier@gmail.com>
2024-07-22 14:00:15 -0500

    imprimir los numeros mayores a 5 de una lista con la programacion tradicional

--- JavaPOO/src/ExpresionesLambda/Clases/Ejecutador.java
+++ JavaPOO/src/ExpresionesLambda/Clases/Ejecutador.java
@@ -1,5 +1,6 @@
 package ExpresionesLambda.Clases;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Ejecutador {
@@ -16,6 +17,24 @@ public class Ejecutador {
 
 //        tambien podemos hacerlo de esta manera
 
-        Arrays.asList(1,2,3,4,5,6).forEach(System.out :: println);
+//        Arrays.asList(1,2,3,4,5,6).forEach(System.out :: println);
+
+
+        // filtrar loas numero de una lista utilizando programacion normal
+        // creamos la lista vacia
+        ArrayList<Integer> listaX = new ArrayList<>();
+        // usamos un for para agregar los datos de la lista arrays.asList
+        for(Integer numero : Arrays.asList(1,2,3,4,5,6,7,8,9)){
+            // si numero es mayor a 5
+            if (numero > 5) {
+                //guardamos eñ numero en la lista
+                listaX.add(numero);
+            }
+        }
+
+        // imprimimos la lista
+        for(Integer valor : listaX){
+            System.out.println(valor);
+        }
     }
 }
