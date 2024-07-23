6640606aafbb721d53dfe35c93e596dd6cebac3d
Javicorporation <krchipullajavier@gmail.com>
2024-07-23 14:59:50 -0500

    Ejercicio de lambda con lista de Alumnos - obtener el alumno nombre de curso tenga un caracter "a", y obtener los alumnos que su nombre tenga mas de 10 caracteres

--- JavaPOO/src/EjerccicioDeLambdas/ejecutador/Ejecutador.java
+++ JavaPOO/src/EjerccicioDeLambdas/ejecutador/Ejecutador.java
@@ -11,13 +11,13 @@ public class Ejecutador {
 
         // cracion de lista
         List<Alumnos> listaDeAlumnos = new ArrayList<>();
-        listaDeAlumnos.add(new Alumnos(1,"948494689","Juanin","carlo","fisica",7.0,21));
+        listaDeAlumnos.add(new Alumnos(1,"948494689","Juanin","carlo","fisic",7.0,21));
         listaDeAlumnos.add(new Alumnos(2, "123456789", "María", "Gómez", "MatemáticasT", 8.5, 22));
-        listaDeAlumnos.add(new Alumnos(3, "987654321", "Luis", "Pérez", "Química", 9.0, 20));
-        listaDeAlumnos.add(new Alumnos(4, "456789123", "lna", "Martínez", "BiologíaT", 6.5, 23));
-        listaDeAlumnos.add(new Alumnos(5, "321654987", "Carlos", "Hernández", "Historia", 7.5, 21));
+        listaDeAlumnos.add(new Alumnos(3, "987654321", "Luis", "Pérez", "Químic", 9.0, 20));
+        listaDeAlumnos.add(new Alumnos(4, "456789123", "lna", "Martínez", "BiologíT", 6.5, 23));
+        listaDeAlumnos.add(new Alumnos(5, "321654987", "Carlos", "Hernández", "Histori", 7.5, 21));
         listaDeAlumnos.add(new Alumnos(6, "654321789", "gofía", "López", "Literatura", 8.0, 22));
-        listaDeAlumnos.add(new Alumnos(7, "789123456", "ledro", "Ramírez", "FilosofíaT", 7.8, 42));
+        listaDeAlumnos.add(new Alumnos(7, "789123456", "ledrolasger", "Ramírez", "FilosofíaT", 7.8, 42));
 
         System.out.println("lista de alumnos: ");
 
@@ -63,5 +63,21 @@ public class Ejecutador {
         listaDeAlumnos.stream()
                 .filter(c -> c.getNombreCurso().toLowerCase().endsWith("t"))
                 .forEach(System.out::println);
+
+        // obtener los alumnos que en el nombre del curso  contenga una a
+        System.out.println("\n------------------");
+        System.out.println("alumnos que en el nombre del curso  contenga una a");
+        listaDeAlumnos.stream()
+                .filter(c -> c.getNombreCurso().toLowerCase().contains("a"))
+                .forEach(System.out::println);
+
+        // obtener los alumnos que su nombre tenga mas de 10 caracteres
+        System.out.println("\n------------------");
+        System.out.println("alumnos que su nombre tenga mas de 10 caracteres");
+
+        listaDeAlumnos.stream()
+                .filter(c -> c.getNombre().length() > 10)
+                .forEach(System.out::println);
+
     }
 }
