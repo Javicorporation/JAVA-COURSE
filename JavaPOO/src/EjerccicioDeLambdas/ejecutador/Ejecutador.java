15a9819f4c5143901ead740844332a4467f8d7ff
Javicorporation <krchipullajavier@gmail.com>
2024-07-23 14:05:47 -0500

    Ejercicio de lambda con lista de Alumnos - obtener alumnos segun su primera letra del nombre

--- JavaPOO/src/EjerccicioDeLambdas/ejecutador/Ejecutador.java
+++ JavaPOO/src/EjerccicioDeLambdas/ejecutador/Ejecutador.java
@@ -14,15 +14,28 @@ public class Ejecutador {
         listaDeAlumnos.add(new Alumnos(1,"948494689","Juanin","carlo","fisica",7.0,21));
         listaDeAlumnos.add(new Alumnos(2, "123456789", "María", "Gómez", "Matemáticas", 8.5, 22));
         listaDeAlumnos.add(new Alumnos(3, "987654321", "Luis", "Pérez", "Química", 9.0, 20));
-        listaDeAlumnos.add(new Alumnos(4, "456789123", "Ana", "Martínez", "Biología", 6.5, 23));
+        listaDeAlumnos.add(new Alumnos(4, "456789123", "lna", "Martínez", "Biología", 6.5, 23));
         listaDeAlumnos.add(new Alumnos(5, "321654987", "Carlos", "Hernández", "Historia", 7.5, 21));
-        listaDeAlumnos.add(new Alumnos(6, "654321789", "Sofía", "López", "Literatura", 8.0, 22));
-        listaDeAlumnos.add(new Alumnos(7, "789123456", "Pedro", "Ramírez", "Filosofía", 7.8, 24));
+        listaDeAlumnos.add(new Alumnos(6, "654321789", "gofía", "López", "Literatura", 8.0, 22));
+        listaDeAlumnos.add(new Alumnos(7, "789123456", "ledro", "Ramírez", "Filosofía", 7.8, 24));
 
         System.out.println("lista de alumnos: ");
 
 
         // convertimos en un flujo de elementos al arrayList por medio del stream
         listaDeAlumnos.stream().forEach(zzz -> System.out.println(" "+zzz));
+
+        // alumnos cuyo nombre empieza con L o G
+        System.out.println("\n------------------");
+        System.out.println("alumnos cuyo nombre empieza con L o G");
+        listaDeAlumnos.stream()
+                .filter(c -> c.getNombre().charAt(0) == 'L' || c.getNombre().charAt(0) == 'G')
+                .forEach(c -> System.out.println(c.toString()));
+
+        // alumnos cuyo nombre empieza con L o G, acepta mayusculas y minusculas
+        System.out.println("\n------------------");
+        System.out.println("alumnos cuyo nombre empieza con L o G");
+        listaDeAlumnos.stream()
+                .filter(c -> Character.toLowerCase(c.getNombre().charAt(0)) == 'l' || Character.toLowerCase(c.getNombre().charAt(0)) == 'g').forEach(c -> System.out.println(c));
     }
 }
