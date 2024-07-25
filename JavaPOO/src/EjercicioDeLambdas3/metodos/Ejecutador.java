37b952d6173b97103a0a3f7f28f50ee6c00d0882
Javicorporation <krchipullajavier@gmail.com>
2024-07-25 14:16:30 -0500

    Ejercicio de lambda 3 - uso de .valueOf - uso de .max y fin del curso basico T-T

--- JavaPOO/src/EjercicioDeLambdas3/metodos/Ejecutador.java
+++ JavaPOO/src/EjercicioDeLambdas3/metodos/Ejecutador.java
@@ -4,6 +4,7 @@ import EjercicioDeLambdas3.clases.Pelicula;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Ejecutador {
@@ -12,7 +13,7 @@ public class Ejecutador {
 
         List<Pelicula> listaPeliculas = new ArrayList<>();
         listaPeliculas.add(new Pelicula("E.t","Steven Spielberg",2002));
-        listaPeliculas.add(new Pelicula("Jurassic Park", "Steven Spielberg", 2019));
+        listaPeliculas.add(new Pelicula("Jurassic Park", "Steven Spielberg", 2029));
         listaPeliculas.add(new Pelicula("Juanin", "Jacintoo", 1979));
         listaPeliculas.add(new Pelicula("El Padrino", "Francis Ford Coppola", 1990));
         listaPeliculas.add(new Pelicula("El Señor de los Anillos", "Peter Jackson", 2010));
@@ -39,11 +40,25 @@ public class Ejecutador {
 
 //        Obtener un sublistado con los nombres de las películas, ordenados alfabéticamente
         System.out.println();
-        System.out.println("");
+        System.out.println("Obtener un sublistado con los nombres de las películas, ordenados alfabéticamente");
+        List<String> nombres = listaPeliculas.stream()
+                .map(v -> v.getTitulo())
+                .sorted((v1,v2) -> v1.compareTo(v2))
+                .collect(Collectors.toList());
+        nombres.forEach(v -> System.out.println(v));
 
 //        Mostrar por pantalla el estreno más reciente
         System.out.println();
-        System.out.println("");
+        System.out.println("Mostrar por pantalla el estreno más reciente");
+        Optional<Pelicula> estrenos= listaPeliculas.stream()
+                .max((v1 , v2) -> Integer.compare(v1.getEstreno(), v2.getEstreno()));
+
+        String respuestaDatosPelicula =
+                (estrenos.isPresent())
+                        ? String.valueOf(estrenos.get())
+                        : "no se ha encontrado";
+        System.out.println("pelicula estrenada");
+        System.out.println(respuestaDatosPelicula);
 
     }
 }
